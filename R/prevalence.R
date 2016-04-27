719abc3f70b143e37661ab35014dbd634a7b4210
Stuart Lacy <stuart.lacy@egu.york.ac.uk>
2016-04-27 10:17:57 +0100

    bar command

--- R/prevalence.R
+++ R/prevalence.R
@@ -94,6 +94,7 @@ prevalence <- function(data, registry_years, N_years,
     #
     # TODO Remove when finished debugging
     # foooooo
+    # baaaaaaaaaar
     #set.seed(17)
     #data = registry_data
     #registry_years = sapply(5:13, function(x) sprintf("20%02d-09-01", x))
