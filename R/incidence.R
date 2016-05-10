d222229a8832a48c7fc279087168af9260bec1eb
Steph Lax <steph.lax@egu.york.ac.uk>
2016-05-10 14:27:50 +0100

    Made all num_years into num_reg_years for consistency

--- R/incidence.R
+++ R/incidence.R
@@ -49,19 +49,19 @@ load_data <- function(data,
                       sex = "Both",
                       age_division = "None",
                       age_cut = 20){
-    
+
     if(!sex %in% c('Both', 'Males', 'Females')) stop("error: incorrect sex.")
     if(sex == "Males") data = filter(data, sex == 0)
     if( sex == "Females") data = filter(data, sex == 1)
-    
+
     if(!age_division %in% c('None', 'Adult', 'Paediatric')) stop("error: incorrect age_division.")
     if(age_division == "Adult") data = filter(data, age >= age_cut)
     if(age_division == "Paediatric") data = filter(data, age < age_cut)
-    
+
     myCols <- c(age_initial, "sex", date_initial, indicator, survival_time, date_event, indicator_censored_at_index)
     data <- data %>%
         select(match(myCols, names(.)))
-    
+
     names(data) <- c("age_initial",
                      "sex",
                      "date_initial",
@@ -76,24 +76,24 @@ load_data <- function(data,
 #'
 #' @param entry Vector of diagnosis dates for each patient in the registry.
 #' @param start Date from which incident cases are included.
-#' @param num_years Integer representing the number of complete years of the registry for which incidence is to be calculated.
+#' @param num_reg_years Integer representing the number of complete years of the registry for which incidence is to be calculated.
 #' @return Vector of absolute incidence values for each included year of the registry.
 #' @examples
 #' incidence(registry_data$DateOfDiag, start="2005-09-01", 8)
-incidence <- function(entry, start=NULL, num_years=NULL) {
-    
+incidence <- function(entry, start=NULL, num_reg_years=NULL) {
+
     if (is.null(start))
         start <- min(entry)
-    
-    if (is.null(num_years)) 
-        num_years <- floor(as.numeric(difftime(max(entry), start) / 365.25))
-    
-    registry_years <- .determine_registry_years(start, num_years)
-    
-    per_year <- vapply(seq(num_years),
+
+    if (is.null(num_reg_years))
+        num_reg_years <- floor(as.numeric(difftime(max(entry), start) / 365.25))
+
+    registry_years <- .determine_registry_years(start, num_reg_years)
+
+    per_year <- vapply(seq(num_reg_years),
                        function(i) sum(entry >= registry_years[i] & entry < registry_years[i+1]),
                        integer(1))
-    
+
     if(sum(per_year) < 30) warning("warning: low number of incident cases.")
     per_year
 }
@@ -111,7 +111,7 @@ incidence <- function(entry, start=NULL, num_years=NULL) {
 incidence_current <- function(data, registry_years, registry_start_year, registry_end_year){
     years_estimated <- registry_end_year - registry_start_year + 1
     per_year <- rep(NA, years_estimated)
-    
+
     for (i in registry_start_year:registry_end_year){
         per_year[i - registry_start_year + 1] <-
             length(data$date_initial[data$date_initial >= registry_years[i] & data$date_initial < registry_years[i + 1]])
@@ -126,29 +126,29 @@ incidence_current <- function(data, registry_years, registry_start_year, registr
 #' @param entry Vector of diagnosis dates for each patient in the registry.
 #' @param population_size Integer representing the size of the population at risk.
 #' @param start Date from which incident cases are included.
-#' @param num_years Integer representing the number of complete years of the registry for which incidence is to be calculated.
+#' @param num_reg_years Integer representing the number of complete years of the registry for which incidence is to be calculated.
 #' @param precision Integer representing the number of decimal places required.
 #' @param level Double representing the desired confidence interval width.
 #' @return List containing mean incidence/year and incidence rate/100,000/year with confidence intervals.
 #' @examples
 #' incidence_rates <- meanIR(load_data(registry_data), registry_years, registry_start_year = 1,
 #' registry_end_year = 4, population_size = 3500000)
-meanIR <- function(entry, population_size, start=NULL, num_years=NULL, precision = 2, level=0.95){
-    
+meanIR <- function(entry, population_size, start=NULL, num_reg_years=NULL, precision = 2, level=0.95){
+
     if (is.null(start))
         start <- min(entry)
-    
-    if (is.null(num_years)) 
-        num_years <- floor(as.numeric(difftime(max(entry), start) / 365.25))
-    
-    registry_years <- .determine_registry_years(start, num_years)
-    
-    mean_rate <- mean(incidence(entry, start=start, num_years=num_years))
+
+    if (is.null(num_reg_years))
+        num_reg_years <- floor(as.numeric(difftime(max(entry), start) / 365.25))
+
+    registry_years <- .determine_registry_years(start, num_reg_years)
+
+    mean_rate <- mean(incidence(entry, start=start, num_reg_years=num_reg_years))
     z_conf <- qnorm((1+level)/2)
-    
-    CI <- 100000 * (z_conf * sqrt(mean_rate) / num_years) / population_size
+
+    CI <- 100000 * (z_conf * sqrt(mean_rate) / num_reg_years) / population_size
     est <- 100000 * mean_rate / population_size
-    
+
     object <- list(absolute=mean_rate, per100K=est, per100K.lower=est-CI, per100K.upper=est+CI)
     lapply(object, round, precision)
 }
@@ -167,15 +167,15 @@ meanIR <- function(entry, population_size, start=NULL, num_years=NULL, precision
 #' incidence_rates <- meanIR_current(load_data(registry_data), registry_years, registry_start_year = 1,
 #' registry_end_year = 4, population_size = 3500000)
 meanIR_current <- function(data, registry_years, registry_start_year, registry_end_year, population_size, precision = 2){
-    
+
     per_year <- incidence_current(data, registry_years, registry_start_year, registry_end_year)
     mean_rate <- mean(per_year)
     CI <- 100000 * (1.96 * sqrt(mean_rate) / (registry_end_year - registry_start_year + 1)) / population_size
     est <- 100000 * mean_rate / population_size
-    
+
     result <- data.frame(round(c(mean_rate, est - CI, est, est + CI),
                                precision))
     rownames(result) <- c("absolute", "per100000-CI", "per100000", "per100000+CI")
     return(result)
-    
+
 }
--- R/utils.R
+++ R/utils.R
@@ -56,11 +56,11 @@ summary.prevalence <- function(object, ...) {
     cat(object$simulated_cases)
 }
 
-.determine_registry_years <- function(start, num_years) {
+.determine_registry_years <- function(start, num_reg_years) {
     # Calculate registry years from this
     # NB: Ugly hack to not take leap years into account. Done so that tests don't throw an error, but strictly
     # should just use as.Date(start) + 365.25 * x to account for leap years
-    sapply(0:num_years, function(x) paste(as.numeric(strftime(start, '%Y'))+x,
+    sapply(0:num_reg_years, function(x) paste(as.numeric(strftime(start, '%Y'))+x,
                                           strftime(start, '%m-%d'), sep='-'))
 }
 
