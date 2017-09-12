1c439b5bc6b1c8e306aa4dcf35df5820cfbd0713
stulacy <stuart.lacy@gmail.com>
2017-09-12 12:09:56 +0100

    Added unit tests for testing predicted survival probabilities for survregmin
    The tests compare the estimated survival probabilities to those generated by flexsurvreg, taking these as the correct versions. Otherwise I'd be implementing my own method a second time.

--- tests/testthat/test_survival_model.R
+++ tests/testthat/test_survival_model.R
@@ -66,3 +66,51 @@ test_that("predict_survival_probability handles incorrectly specified arguments"
 
 
 })
+
+test_that("predict_survival_probability produces correct survival estimates", {
+
+    # For this test, a "correct" survival estimate is provided by the flexsurv
+    # package which is a wrapper around survreg. Since survreg doesn't provide a
+    # function for calculating S(t) from a model, I'd have to write my own implementation
+    # which is essentially what I've done for predict_survival_probability.survregmin
+    # so I'd be comparing two of my own implementations against each other. It makes more
+    # sense to test against flexsurv
+    test_surv <- function(form, dist, newdata, time) {
+        mod <- build_survreg(form, prevsim, dist)
+        flexmod <- flexsurv::flexsurvreg(form, data=prevsim, dist=dist)
+
+        survregmin_pred <- as.numeric(predict_survival_probability(mod, newdata, time))
+
+        # Need ugly lapply here as by default flexsurv creates estimate for each individual for each t
+        ndataflex <- if (nrow(newdata) == 0) NULL else newdata
+        flexsurv_raw <- summary(flexmod, newdata=ndataflex, t=time)
+        flexsurv_pred <- as.numeric(unlist(lapply(1:length(time), function(t) flexsurv_raw[[t]][t, ][2])))
+
+        expect_equal(survregmin_pred, flexsurv_pred)
+    }
+
+    # build models on all combinations of covars and dists
+    test_surv(Surv(time, status) ~ 1, 'weibull', data.frame(), time=500)
+    test_surv(Surv(time, status) ~ 1, 'lognormal', data.frame(), time=58)
+    test_surv(Surv(time, status) ~ 1, 'exponential', data.frame(), time=40)
+
+    test_surv(Surv(time, status) ~ age, 'weibull', data.frame(age=30), time=500)
+    test_surv(Surv(time, status) ~ age, 'lognormal', data.frame(age=50), time=58)
+    test_surv(Surv(time, status) ~ age, 'exponential', data.frame(age=c(70, 84)), time=c(40, 60))
+
+    test_surv(Surv(time, status) ~ sex, 'weibull', data.frame(sex=factor(c('F', 'M'), levels=levels(prevsim$sex))), time=c(1000, 232))
+    test_surv(Surv(time, status) ~ sex, 'lognormal', data.frame(sex=factor('M', levels=levels(prevsim$sex))), time=58)
+    test_surv(Surv(time, status) ~ sex, 'exponential', data.frame(sex=factor(c('M', 'F'), levels=levels(prevsim$sex))), time=c(40, 60))
+
+    test_surv(Surv(time, status) ~ sex + age, 'weibull', data.frame(age=50, sex=factor('M', levels=levels(prevsim$sex))), time=423)
+    test_surv(Surv(time, status) ~ sex + age,
+              'lognormal',
+              data.frame(age=rnorm(3, 60, 20),
+                         sex=factor(c('F', 'M', 'M'), levels=levels(prevsim$sex))),
+              time=c(10, 232, 5000))
+    test_surv(Surv(time, status) ~ sex + age, # Test on 23 patients
+              'exponential',
+              data.frame(age=rnorm(23, 80, 10),
+                         sex=factor(sample(c('M', 'F'), 23, replace=T), levels=levels(prevsim$sex))),
+              time=runif(23, 0, 4000))
+})
