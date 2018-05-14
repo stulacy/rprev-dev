c3d6c5726e723beb6b19fd8b942b2b71369555ac
stulacy <stuart.lacy@gmail.com>
2018-05-14 14:01:31 +0100

    Got implementation of fixed cure model working

--- R/fixedcure.R
+++ R/fixedcure.R
@@ -19,9 +19,6 @@ fixed_cure <- function(formula, data, cure_time, daily_survival=NULL, population
 
     func_call <- match.call()
     func_call$formula <- eval(formula)
-    func_call$dist <- eval(dist)
-    func_call$time <- eval(time)
-    func_call$daily_survival <- eval(daily_survival)
     obj$call <- func_call
 
     if (is.null(daily_survival)) {
@@ -37,7 +34,7 @@ fixed_cure <- function(formula, data, cure_time, daily_survival=NULL, population
     obj$pop_covars <- mortality_covars
 
     obj$pop_data <- daily_survival
-    obj$cure_time <- time
+    obj$cure_time <- cure_time
 
     class(obj) <- 'fixedcure'
 
@@ -54,33 +51,54 @@ predict_survival_probability.fixedcure <- function(object, newdata, times) {
     surv.index <- NULL
     surv.cure <- NULL
 
-    browser()
-    raw_probs <- predict_survival_probability.survregmin(object, newdata, times)
-    pop_indices <- setNames(object$pop_covars, object$pop_covars)
+    # Calculate raw survival time
+    probs <- newdata %>%
+        dplyr::mutate(id = 1:nrow(newdata),
+                      time_to_index = times,
+                      cured = time_to_index > object$cure_time,
+                      time_calc_survival = ifelse(cured, object$cure_time, time_to_index),
+                      surv_prob = predict_survival_probability.survregmin(object, ., time_calc_survival))
 
-    cured_individuals <- newdata %>%
-        dplyr::mutate(time_to_index = times,
-                      raw_prob = raw_probs) %>%
-        dplyr::filter(time_to_index > object$cure_time)
+    if (sum(probs$cured) > 0) {
+        # Cured survival probabilty is defined by Simon as
+        # S(t) = S(curetime) * S*(t) / S*(curetime)
+        # Where S*(.) is population survival rates calculated in terms of a person's age
+        # S(curetime) was already calculated for these cured patients above and is held in 'surv_prob'
 
-    if (nrow(cured_individuals) > 0) {
+        pop_indices <- setNames(object$pop_covars, object$pop_covars)
 
-        cured_individuals <- cured_individuals %>%
-            dplyr::mutate(age_at_cure = floor(age * DAYS_IN_YEAR + object$cure_time),
-                          age_at_index = floor(age * DAYS_IN_YEAR + time_to_index))
-        # TODO How to get this working for people who have age at cure or age at index > max age in mortality data? Assume dead?
-        adj_probs <- cured_individuals %>%
-                dplyr::left_join(object$pop_data, by=c('age_at_cure'='age', pop_indices)) %>%
-                dplyr::left_join(object$pop_data, by=c('age_at_index'='age', pop_indices), suffix=c('.cure', '.index')) %>%
-                dplyr::mutate(adj_prob = raw_prob * (surv.index / surv.cure))
+        # Calculate the population survival rates by linking to the population survival rates
+        cured_probs <- probs %>%
+                    dplyr::filter(cured) %>%
+                    dplyr::mutate(age_at_cure = floor(age * DAYS_IN_YEAR + object$cure_time),
+                                  age_at_index = floor(age * DAYS_IN_YEAR + time_to_index))
 
-        raw_probs[times > object$cure_time] <- adj_probs$adj_prob
+        # Suppress warnings when joining by character field
+        cured_probs_2 <- suppressWarnings(dplyr::left_join(cured_probs, object$pop_data, by=c('age_at_cure'='age', pop_indices)))
+        cured_probs_3 <- suppressWarnings(dplyr::left_join(cured_probs_2, object$pop_data, by=c('age_at_index'='age', pop_indices),
+                                                           suffix=c('.cure', '.index')))
+        cured_probs_4 <- cured_probs_3 %>%
+                            dplyr::mutate(adj_prob = surv_prob * (surv.index / surv.cure),
+                                          adj_prob = ifelse(is.na(adj_prob), 0, adj_prob))
+
+        # Link back with all newdata
+        probs <- probs %>%
+                    dplyr::left_join(cured_probs_4 %>% dplyr::select(id, adj_prob), by='id') %>%
+                    dplyr::mutate(surv_prob = ifelse(cured, adj_prob, surv_prob))
     }
 
-    raw_probs
+    # Return a vector
+    probs %>% dplyr::pull(surv_prob)
 }
 
 #' @export
 extract_covars.fixedcure <- function(object) {
     object$covars
+}
+
+print.fixedcure <- function(object, ...) {
+    to_print <- object[c('coefs', 'covars', 'call', 'dist', 'terms', 'pop_covars', 'cure_time')]
+    to_print$population_survival <- summary(object$pop_data)
+    print(to_print)
+
 }
